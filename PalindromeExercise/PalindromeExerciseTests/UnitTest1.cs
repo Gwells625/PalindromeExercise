b694ab320e15b873f47433476b6b97552547968b
Geoffrey Wells <Gwells625@gmail.com>
2025-07-15 15:59:58 -0400

    Completed exercise.

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        
+            [Theory]
+            [InlineData("kayak", true)]
+            [InlineData("Goodbye", false)]
+            public void TestPalindrome(string word, bool expected)
+            {
+                //Arrange - 
+                var tester = new WordSmith();
 
+                //Act
+                var actual = tester.IsAPalindrome(word);
+
+                //Assert
+                Assert.Equal(expected, actual);
+
+            }
         }
     }
-}
